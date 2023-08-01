86b96a9d66e2637ca5881385d2bdd1b99e49698a
edsonpag <edsonsilvapagani@gmail.com>
2023-08-01 14:19:16 -0300

    ajustes no nodemailer

--- src/controller/EmailController.js
+++ src/controller/EmailController.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
 
-const send = (req, res) => {
+const send = async (req, res) => {
     const mailOptions = req.body;
-    nodemailer.createTransport({
+    const transporter = await  nodemailer.createTransport({
         host: "smtp.hostinger.com",
         port: 465,
         secure: true,
@@ -10,13 +10,18 @@ const send = (req, res) => {
             user: process.env.DIGITAL_QUIZ_EMAIL,
             pass: process.env.DIGITAL_QUIZ_PASSWORD
         }
-    }).sendMail(mailOptions, function(err, data) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("Email enviado com sucesso");
-        }
     });
+    await new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, function(err, data) {
+            if (err) {
+                console.log(err)
+                reject(err)
+            } else {
+                console.log("Email enviado com sucesso");
+                resolve(data)
+            }
+        });
+    })
     res.json({
         success: true,
         msg: "ok"
